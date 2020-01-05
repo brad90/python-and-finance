c19e7fd6da38a7ce6f277f3b80c38d1f0874ccdc
Stephen Bradley <stephen.a.bradley90@outlook.com>
2020-01-05 09:59:08 +0000

    Ploting graph in alternate way

--- finance.py
+++ finance.py
@@ -6,14 +6,33 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
+
+
+# Converting stock data from "yahoo" into a csv
+
 # start = dt.datetime(2000,1,1)
 # end = dt.datetime(2016,12,31)
-#
 # df = web.DataReader('TSLA', 'yahoo', start, end)
 # df.to_csv('tsla.csv')
 
-df = pd.read_csv('tsla.csv', parse_dates = True, index_col=0)
-# print(df.head())
 
-df['Adj Close'].plot()
+#  Reading the csv for it to plot.
+df = pd.read_csv('tsla.csv', parse_dates = True, index_col=0)
+
+# df['Adj Close'].plot()
+# plt.show()
+
+# Manipulating the data that is coming form CSV.
+# df['100ma']  Create a new column in the CSV file.
+
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+print(df.tail())
+
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan = 5, colspan = 1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan = 5, colspan = 1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
 plt.show()

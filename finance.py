d762d1b5bb16676a3f9d743c5f14828ad8fa486f
Stephen Bradley <stephen.a.bradley90@outlook.com>
2020-01-05 10:18:32 +0000

    Plotting a graph using candlesticks

--- finance.py
+++ finance.py
@@ -1,6 +1,8 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
+import mplfinance as mpf
+import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
 
@@ -18,6 +20,10 @@ style.use('ggplot')
 
 #  Reading the csv for it to plot.
 df = pd.read_csv('tsla.csv', parse_dates = True, index_col=0)
+df.index.name = 'Date'
+df.shape
+df.head(3)
+df.tail(3)
 
 # df['Adj Close'].plot()
 # plt.show()
@@ -25,14 +31,27 @@ df = pd.read_csv('tsla.csv', parse_dates = True, index_col=0)
 # Manipulating the data that is coming form CSV.
 # df['100ma']  Create a new column in the CSV file.
 
-df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
-print(df.tail())
+# df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+# print(df.tail())
 
-ax1 = plt.subplot2grid((6,1), (0,0), rowspan = 5, colspan = 1)
-ax2 = plt.subplot2grid((6,1), (5,0), rowspan = 5, colspan = 1)
+df_ohlc = df['Adj Close'].resample('10D').ohlc()
+df_volume = df['Volume'].resample('10D').sum()
 
-ax1.plot(df.index, df['Adj Close'])
-ax1.plot(df.index, df['100ma'])
-ax2.bar(df.index, df['Volume'])
+print(df)
+
+# df_ohlc.reset_index(inplace=True)
+
+
+
+mpf.plot(df,type='candle')
+
+
+
+# ax1 = plt.subplot2grid((6,1), (0,0), rowspan = 5, colspan = 1)
+# ax2 = plt.subplot2grid((6,1), (5,0), rowspan = 5, colspan = 1)
+
+# ax1.plot(df.index, df['Adj Close'])
+# ax1.plot(df.index, df['100ma'])
+# ax2.bar(df.index, df['Volume'])
 
 plt.show()
